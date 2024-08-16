0672d04704c6ce6ebc5ce415948d6411a32ed10c
teshager <teshager8922@gmail.com>
2024-08-16 08:11:56 +0300

    incorporate the recursive method into the class

--- main.js
+++ main.js
@@ -2,4 +2,6 @@ import Chess from "./chess.js";
 
 const chess= new Chess(8);
 
-chess.knightMoves([7,7],[0,0])
+// chess.knightMoves([7,7],[0,0])
+
+console.log(chess.knightMoves_recursive([0,0],[3,3]))
